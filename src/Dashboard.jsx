d692bc26e35e83c82dc5bdc3fc77d4f4a6203603
Chunyue Ma <cma01@wesleyan.edu>
2021-04-24 12:05:12 -0400

    connect dashboard and progress bar

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Progress from './Progress';
 
-const Dashboard = () => <div></div>;
+const Dashboard = () => {
+  return <Progress />;
+};
 
 export default Dashboard;
