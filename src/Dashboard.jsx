5a1b1c57b7e62698f67a3af87e77c55af03e951b
Chunyue Ma <cma01@wesleyan.edu>
2021-04-24 12:48:12 -0400

    add props to progress

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import Progress from './Progress';
+import Cookies from 'universal-cookie';
 
+// dashboard should contain the dial and the progress bar
 const Dashboard = () => {
-  return <Progress />;
+  const cookies = new Cookies();
+  const username = cookies.get('appUsername');
+  const amountGoal = 20;
+  const amountLeft = 15;
+  const percentageLeft = amountLeft / amountGoal;
+  return <Progress amountGoal={amountGoal} amountLeft={amountLeft} />;
 };
 
 export default Dashboard;
