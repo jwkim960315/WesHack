e3b54134243da09ddb137fe493c55b7100b98e9d
Chunyue Ma <cma01@wesleyan.edu>
2021-04-24 00:39:40 -0400

    progress-bar-component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Progress from './Progress';
+
+function App() {
+  return (
+    <div className="App">
+      <p> This is our amazing gauge! </p>
+      <Progress />
+    </div>
+  );
+}
+
+export default App;
