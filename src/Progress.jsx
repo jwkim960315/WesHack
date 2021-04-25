d74b8e9acfbd287961006715f75155705b0b87e1
Jung Woo (Sean) Kim <jkim11@wesleyan.edu>
2021-04-25 14:15:38 -0400

    fix responsiveness

--- src/Progress.jsx
+++ src/Progress.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-
+import { Grid, Typography } from '@material-ui/core';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
@@ -14,72 +14,67 @@ const Progress = ({ goal, left, moneyUnit, timeUnit, valuePercentage }) => {
 
   if (goal && left !== null) {
     return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%'
-        }}
-      >
-        <div
-          style={{
-            fontSize: 20,
-            color: '#ff5722'
-          }}
-        >
-          Current Plan
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            margin: 'auto',
-            position: 'relative',
-            justifyContent: 'center'
-          }}
-        >
-          <div style={{ width: 350, height: 350 }}>
-            <ProgressProvider valueEnd={valueEnd}>
-              {value => (
-                <>
-                  <div
-                    style={{
-                      position: 'absolute',
-                      top: '40%',
-                      left: '27%',
-                      color: '#228B22',
-                      marginTop: '10%',
-                      fontSize: '1.4em'
-                    }}
-                  >
-                    {moneyUnit + ` left`}
+      <>
+        <Grid container justify="center" alignItems="center" spacing={2}>
+          <Grid item xs={12}>
+            <Typography
+              component="div"
+              color="primary"
+              style={{ fontSize: 20 }}
+            >
+              Current Plan
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <div
+              style={{
+                maxHeight: 350,
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center'
+              }}
+            >
+              <ProgressProvider valueEnd={valueEnd}>
+                {value => (
+                  <div style={{ position: 'relative' }}>
+                    <div
+                      style={{
+                        position: 'absolute',
+                        top: '48%',
+                        left: '45%',
+                        color: '#228B22',
+                        marginTop: '10%',
+                        fontSize: '1.4em'
+                      }}
+                    >
+                      {moneyUnit + ` left`}
+                    </div>
+                    <CircularProgressbar
+                      value={value}
+                      text={`${left}/${goal}`}
+                      styles={{
+                        // Customize the path, i.e. the "completed progress"
+                        path: {
+                          stroke: `rgba(255, 255, 255, ${value / 100})`,
+                          transition: 'stroke-dashoffset 0.5s ease 0s'
+                        },
+                        // Customize the circle behind the path, i.e. the "total progress"
+                        trail: {
+                          // Trail color
+                          stroke: '#228B22'
+                        },
+                        // Customize the text
+                        text: {
+                          fill: '#228B22'
+                        }
+                      }}
+                    />
                   </div>
-                  <CircularProgressbar
-                    value={value}
-                    text={`${left}/${goal}`}
-                    styles={{
-                      // Customize the path, i.e. the "completed progress"
-                      path: {
-                        stroke: `rgba(255, 255, 255, ${value / 100})`,
-                        transition: 'stroke-dashoffset 0.5s ease 0s'
-                      },
-                      // Customize the circle behind the path, i.e. the "total progress"
-                      trail: {
-                        // Trail color
-                        stroke: '#228B22'
-                      },
-                      // Customize the text
-                      text: {
-                        fill: '#228B22'
-                        // fontSize: '14px'
-                      }
-                    }}
-                  />
-                </>
-              )}
-            </ProgressProvider>
-          </div>
-          <div style={{ marginLeft: 30, width: 250 }}>
+                )}
+              </ProgressProvider>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={6}>
             <div
               style={{
                 padding: 10,
@@ -117,9 +112,18 @@ const Progress = ({ goal, left, moneyUnit, timeUnit, valuePercentage }) => {
                 </li>
               </ul>
             </div>
-          </div>
-        </div>
-      </div>
+          </Grid>
+        </Grid>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            margin: 'auto',
+            position: 'relative',
+            justifyContent: 'center'
+          }}
+        ></div>
+      </>
     );
   } else {
     return <div></div>;
