761a39ecf05799042df150783a01cb38fb8a8579
jwkim960315 <jkim11@wesleyan.edu>
2021-04-25 12:47:06 -0400

    fixing responsiveness

--- src/Dial.jsx
+++ src/Dial.jsx
@@ -1,52 +1,50 @@
 // import { rgbToHex } from "@material-ui/core";
 // import { SportsRugbySharp } from "@material-ui/icons";
-import React, { useState, useEffect, useRef } from "react";
-import ReactSpeedometer from "react-d3-speedometer";
+import React, { useState, useEffect, useRef } from 'react';
+import ReactSpeedometer from 'react-d3-speedometer';
+import { Grid } from '@material-ui/core';
 
 const Dial = ({ goal, past, timeUnit, moneyUnit, spendingRatio }) => {
   const dialWrapperRef = useRef(null);
   const [rerender, setRerender] = useState(false);
 
   useEffect(() => {
-    setRerender((prevRerender) => !prevRerender);
+    setRerender(prevRerender => !prevRerender);
   }, [dialWrapperRef]);
-
-  // var spendingRatio = past / goal;
-  // var spendingDisplay = Math.min(2, spendingRatio);
 
   const spending = () => {
     if (spendingRatio > 1.75) {
-      return "Really Overspending";
+      return 'Really Overspending';
     } else if (spendingRatio > 1.25) {
-      return "Overspending";
+      return 'Overspending';
     } else if (spendingRatio > 1.05) {
-      return "Slightly Overspending";
+      return 'Slightly Overspending';
     } else if (spendingRatio > 0.95) {
-      return "Spending Right";
+      return 'Spending Right';
     } else if (spendingRatio > 0.75) {
-      return "Slightly Underspending";
+      return 'Slightly Underspending';
     } else if (spendingRatio > 0.25) {
-      return "Underspending";
+      return 'Underspending';
     } else {
-      return "Really Underspending";
+      return 'Really Underspending';
     }
   };
 
   const getColor = () => {
     if (spendingRatio > 1.75) {
-      return "#fd7d1c";
+      return '#fd7d1c';
     } else if (spendingRatio > 1.25) {
-      return "#e9f922";
+      return '#e9f922';
     } else if (spendingRatio > 1.05) {
-      return "#9df725";
+      return '#9df725';
     } else if (spendingRatio > 0.95) {
-      return "#55f428";
+      return '#55f428';
     } else if (spendingRatio > 0.75) {
-      return "#2af239";
+      return '#2af239';
     } else if (spendingRatio > 0.25) {
-      return "#2eef97";
+      return '#2eef97';
     } else {
-      return "#33dfec";
+      return '#33dfec';
     }
   };
 
@@ -54,32 +52,24 @@ const Dial = ({ goal, past, timeUnit, moneyUnit, spendingRatio }) => {
     const { innerWidth: width, innerHeight: height } = window;
     return {
       width,
-      height,
+      height
     };
   }
 
   if (spendingRatio) {
     return (
-      <div
-        style={{
-          height: "100%",
-          display: "flex",
-          justifyContent: "space-between",
-          margin: "auto",
-          alignItems: "center",
-        }}
-      >
-        <div ref={dialWrapperRef}>
+      <Grid container justify="center" alignItems="center" spacing={1}>
+        <Grid item>
           <ReactSpeedometer
-            startColor={"#34c9eb"}
-            endColor={"#FF471A"}
+            startColor={'#34c9eb'}
+            endColor={'#FF471A'}
             width={300}
             height={200}
             maxSegmentLabels={0}
             segments={51}
             value={Math.min(2, spendingRatio)}
             currentValueText={spending()}
-            valueTextFontSize={"30px"}
+            valueTextFontSize={'30px'}
             maxValue={2}
             ringWidth={60}
             textColor={getColor()}
@@ -87,27 +77,27 @@ const Dial = ({ goal, past, timeUnit, moneyUnit, spendingRatio }) => {
             paddingVertical={20}
             forceRender={true}
           />
-        </div>
-        <div style={{ marginLeft: 20 }}>
+        </Grid>
+        <Grid item>
           <div
             style={{
               width: 250,
-              margin: "auto",
+              margin: 'auto',
               padding: 10,
-              backgroundColor: "#555",
+              backgroundColor: '#555'
             }}
           >
-            Based on the past month's data, you have a{" "}
+            Based on the past month's data, you have a{' '}
             <span style={{ color: getColor() }} className="predicted">
               predicted
-            </span>{" "}
+            </span>{' '}
             spending rate of
             <span style={{ color: getColor() }} className="predicted">
-              {" " +
+              {' ' +
                 past +
-                " " +
+                ' ' +
                 moneyUnit.toLowerCase() +
-                " per " +
+                ' per ' +
                 timeUnit.toLowerCase()}
             </span>
           </div>
@@ -116,18 +106,86 @@ const Dial = ({ goal, past, timeUnit, moneyUnit, spendingRatio }) => {
               marginTop: 20,
               width: 250,
               padding: 10,
-              backgroundColor: "#555",
+              backgroundColor: '#555'
             }}
           >
-            For this {timeUnit.toLowerCase()}, it is{" "}
+            For this {timeUnit.toLowerCase()}, it is{' '}
             <span className="suggestion">suggested </span>
-            you spend approximately{" "}
+            you spend approximately{' '}
             <span className="suggestion">
-              {goal + " " + moneyUnit.toLowerCase()}
+              {goal + ' ' + moneyUnit.toLowerCase()}
             </span>
           </div>
-        </div>
-      </div>
+        </Grid>
+      </Grid>
+      // <div
+      //   style={{
+      //     height: "100%",
+      //     display: "flex",
+      //     justifyContent: "space-between",
+      //     margin: "auto",
+      //     alignItems: "center",
+      //   }}
+      // >
+      //   <div ref={dialWrapperRef}>
+      //     <ReactSpeedometer
+      //       startColor={"#34c9eb"}
+      //       endColor={"#FF471A"}
+      //       width={300}
+      //       height={200}
+      //       maxSegmentLabels={0}
+      //       segments={51}
+      //       value={Math.min(2, spendingRatio)}
+      //       currentValueText={spending()}
+      //       valueTextFontSize={"30px"}
+      //       maxValue={2}
+      //       ringWidth={60}
+      //       textColor={getColor()}
+      //       paddingHorizontal={20}
+      //       paddingVertical={20}
+      //       forceRender={true}
+      //     />
+      //   </div>
+      //   <div style={{ marginLeft: 20 }}>
+      //     <div
+      //       style={{
+      //         width: 250,
+      //         margin: "auto",
+      //         padding: 10,
+      //         backgroundColor: "#555",
+      //       }}
+      //     >
+      //       Based on the past month's data, you have a{" "}
+      //       <span style={{ color: getColor() }} className="predicted">
+      //         predicted
+      //       </span>{" "}
+      //       spending rate of
+      //       <span style={{ color: getColor() }} className="predicted">
+      //         {" " +
+      //           past +
+      //           " " +
+      //           moneyUnit.toLowerCase() +
+      //           " per " +
+      //           timeUnit.toLowerCase()}
+      //       </span>
+      //     </div>
+      //     <div
+      //       style={{
+      //         marginTop: 20,
+      //         width: 250,
+      //         padding: 10,
+      //         backgroundColor: "#555",
+      //       }}
+      //     >
+      //       For this {timeUnit.toLowerCase()}, it is{" "}
+      //       <span className="suggestion">suggested </span>
+      //       you spend approximately{" "}
+      //       <span className="suggestion">
+      //         {goal + " " + moneyUnit.toLowerCase()}
+      //       </span>
+      //     </div>
+      //   </div>
+      // </div>
     );
   } else {
     return <div></div>;
--- src/Progress.jsx
+++ src/Progress.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 
-import { CircularProgressbar } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
-import ProgressProvider from "./ProgressProvider";
+import ProgressProvider from './ProgressProvider';
 
 const Progress = ({ goal, left, moneyUnit, timeUnit, valuePercentage }) => {
   const [valueEnd, setValueEnd] = React.useState(valuePercentage);
@@ -16,22 +16,22 @@ const Progress = ({ goal, left, moneyUnit, timeUnit, valuePercentage }) => {
     return (
       <div
         style={{
-          padding: "10px 60px 60px 60px",
-          margin: "auto",
-          position: "relative",
+          // padding: "10px 60px 60px 60px",
+          margin: 'auto',
+          position: 'relative'
         }}
       >
         <ProgressProvider valueEnd={valueEnd}>
-          {(value) => (
+          {value => (
             <>
               <div
                 style={{
-                  position: "absolute",
-                  top: "40%",
-                  left: "35%",
-                  color: "#228B22",
-                  marginTop: "10%",
-                  fontSize: "1.4em",
+                  position: 'absolute',
+                  top: '48%',
+                  left: '45%',
+                  color: '#228B22',
+                  marginTop: '10%',
+                  fontSize: '1.4em'
                 }}
               >
                 {moneyUnit + ` left`}
@@ -43,18 +43,18 @@ const Progress = ({ goal, left, moneyUnit, timeUnit, valuePercentage }) => {
                   // Customize the path, i.e. the "completed progress"
                   path: {
                     stroke: `rgba(255, 255, 255, ${value / 100})`,
-                    transition: "stroke-dashoffset 0.5s ease 0s",
+                    transition: 'stroke-dashoffset 0.5s ease 0s'
                   },
                   // Customize the circle behind the path, i.e. the "total progress"
                   trail: {
                     // Trail color
-                    stroke: "#228B22",
+                    stroke: '#228B22'
                   },
                   // Customize the text
                   text: {
-                    fill: "#228B22",
+                    fill: '#228B22'
                     // fontSize: '14px'
-                  },
+                  }
                 }}
               />
             </>
@@ -69,7 +69,7 @@ const Progress = ({ goal, left, moneyUnit, timeUnit, valuePercentage }) => {
 
 function Wrapper(props) {
   return (
-    <div style={{ width: 200, position: "relative" }}>{props.children}</div>
+    <div style={{ width: 200, position: 'relative' }}>{props.children}</div>
   );
 }
 
